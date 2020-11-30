5137974bd88ac22c9daa4c2119cbc84c763ffc30
aryanNaik123 <aryannnaik@outlook.com>
2020-11-30 16:01:25 +0000

    added search

--- app.py
+++ app.py
@@ -6,12 +6,17 @@ from datetime import datetime
 from datetime import timedelta
 import time 
 
-st.title('My first app')
-st.write('Below is a table of data for...')
+df = pd.read_csv('2020 November General Election - Turnout Rates.csv')
+st.title('2020 General Election Turnout')
+st.write('Select State')
+state = st.selectbox('state',options=df.State.unique())
 start_time = st.slider(
 "When do you start?",
 min_value=datetime(1980, 1, 1),
 max_value=datetime(2020, 1, 1),
 format="MM/DD/YY")
 st.write("Start time:", start_time.strftime("%D"))
-#pd.read_csv('')
+if state == 'United States': 
+    st.dataframe(data=df)
+else: 
+    df.loc[df['State']==state]
