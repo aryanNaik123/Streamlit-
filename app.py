1c174002daa8279588aefab77b1a9dbaf458bc55
aryanNaik123 <aryannnaik@outlook.com>
2020-11-30 15:33:13 +0000

    app.py slider

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
 import streamlit as st
+import pandas as pd 
+import numpy 
+import urllib.request 
+from datetime import datetime 
+from datetime import timedelta
+import time 
+
 st.title('My first app')
+st.write('Below is a table of data for...')
+start_time = st.slider(
+"When do you start?",
+min_value=datetime(1980, 1, 1),
+max_value=datetime(2020, 1, 1),
+format="MM/DD/YY")
+st.write("Start time:", start_time.strftime("%D"))
+#pd.read_csv('')
